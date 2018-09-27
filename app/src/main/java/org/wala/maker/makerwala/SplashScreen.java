201917d95133af47ee997a30d8fbd4afe95069c7
rocket17 <soumyajeet1998@gmail.com>
2018-09-28 00:06:39 +0530

    Initial commit

---
+++ app/src/main/java/org/wala/maker/makerwala/SplashScreen.java
@@ -0,0 +1,29 @@
+package org.wala.maker.makerwala;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends Activity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+
+  }
